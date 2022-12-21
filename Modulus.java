a6565e84bc1dd9229650f59274f3f55ba7e9ae20
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-12-21 23:43:36 +0530

    import java.util.*; class Modulus{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c;         c=(int)(a%b);         System.out.print(c);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Modulus{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
+        c=(int)(a%b);
+        System.out.print(c);
+    }
+}
