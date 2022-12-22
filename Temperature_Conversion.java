930473a3f1a182e0f3ca1ae566b939400b0940f9
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-12-22 06:48:31 +0530

    import java.util.*; class Temp{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         float t;         t=32+((float)(x*9)/5);         System.out.format("%.2f",t);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Temp{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        float t;
+        t=32+((float)(x*9)/5);
+        System.out.format("%.2f",t);
+    }
+}
