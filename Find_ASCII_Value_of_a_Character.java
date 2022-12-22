bedda2ac3f922c960f54a3fa74b7511502bc925b
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2022-12-22 06:28:01 +0530

    import java.util.*; class Ascii{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char x;         int y;         x=sc.next().charAt(0);         y=(int)x;         System.out.println(y);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Ascii{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char x;
+        int y;
+        x=sc.next().charAt(0);
+        y=(int)x;
+        System.out.println(y);
+    }
+}
